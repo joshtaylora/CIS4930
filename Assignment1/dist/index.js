7e0b4faf10d626b89e27584cc5f94d791c400411
joshtaylora <joshuataylora@gmail.com>
2021-01-21 19:53:38 -0500

    in-progress: implementing the star rating system

--- Assignment1/dist/index.js
+++ Assignment1/dist/index.js
@@ -1,14 +1,71 @@
 "use strict";
+// the name text input field
+var txtUserName = document.getElementById('txtUserName');
+var txtComments = document.getElementById('txtComments');
+var btnStar1 = document.getElementById('star1btn');
+var btnStar2 = document.getElementById('star2btn');
+var btnStar3 = document.getElementById('star3btn');
+var btnStar4 = document.getElementById('star4btn');
+var btnStar5 = document.getElementById('star5btn');
+var checkedStars = 0;
+btnStar1.addEventListener('click', function (event) {
+    checkedStars = 1;
+});
+btnStar2.addEventListener('click', function (event) {
+    checkedStars = 2;
+});
+btnStar3.addEventListener('click', function (event) {
+    checkedStars = 3;
+});
+btnStar4.addEventListener('click', function (event) {
+    checkedStars = 4;
+});
+btnStar5.addEventListener('click', function (event) {
+    checkedStars = 5;
+});
 var btnSubmit = document.getElementById('btnSubmit');
-var textUserName = document.getElementById('textUserName');
-var dateDOB = document.getElementById('dateDOB');
-var template = getTemplateClone('responseTemplate');
 var responsePoint = document.getElementById('responsePoint');
 btnSubmit.addEventListener('click', function (event) {
-    var enteredDate = new Date(dateDOB.value);
+    var template = getTemplateClone('responseTemplate');
+    var name = txtUserName.value;
+    var comment = txtComments.value;
     var currentDate = new Date();
-    var age = currentDate.getFullYear() - enteredDate.getFullYear();
-    template.content.getElementById('ageOfCurrentUser').innerText = age.toString();
+    var dateD = currentDate.getDay().toString();
+    var dateM = currentDate.getMonth().toString();
+    var dateY = currentDate.getFullYear().toString();
+    var dateString = dateD + '/' + dateM + '/' + dateY;
+    template.content.getElementById('responseName').innerText = name;
+    template.content.getElementById('responseComment').innerText = comment;
+    template.content.getElementById('responseDate').innerText = dateString;
+    switch (checkedStars) {
+        case (1):
+            template.content.getElementById('responseStar1').style.backgroundColor = 'rgb(255,200,0)';
+            break;
+        case (2):
+            template.content.getElementById('responseStar1').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar2').style.backgroundColor = 'rgb(255,200,0)';
+            break;
+        case (3):
+            template.content.getElementById('responseStar1').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar2').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar3').style.backgroundColor = 'rgb(255,200,0)';
+            break;
+        case (4):
+            template.content.getElementById('responseStar1').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar2').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar3').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar4').style.backgroundColor = 'rgb(255,200,0)';
+            break;
+        case (5):
+            template.content.getElementById('responseStar1').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar2').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar3').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar4').style.backgroundColor = 'rgb(255,200,0)';
+            template.content.getElementById('responseStar5').style.backgroundColor = 'rgb(255,200,0)';
+            break;
+        default:
+            break;
+    }
     responsePoint === null || responsePoint === void 0 ? void 0 : responsePoint.appendChild(template.content);
 });
 function getTemplateClone(templateID) {
--- Assignment1/src/index.ts
+++ Assignment1/src/index.ts
@@ -1,27 +1,102 @@
 
+// the name text input field
+var txtUserName = document.getElementById('txtUserName') as HTMLInputElement;
+var txtComments = document.getElementById('txtComments') as HTMLInputElement;
 
-var btnSubmit = document.getElementById('btnSubmit') as HTMLButtonElement;
+var btnStar1 = document.getElementById('star1btn') as HTMLInputElement;
+var btnStar2 = document.getElementById('star2btn') as HTMLInputElement;
+var btnStar3 = document.getElementById('star3btn') as HTMLInputElement;
+var btnStar4 = document.getElementById('star4btn') as HTMLInputElement;
+var btnStar5 = document.getElementById('star5btn') as HTMLInputElement;
 
-var textUserName = document.getElementById('textUserName') as HTMLInputElement;
+var checkedStars:number = 0;
 
-var dateDOB = document.getElementById('dateDOB') as HTMLInputElement;
+btnStar1.addEventListener('click',
+(event) =>
+{
+	checkedStars = 1;
+});
 
-var template = getTemplateClone('responseTemplate');
+btnStar2.addEventListener('click',
+(event) =>
+{
+	checkedStars = 2;
+});
+
+btnStar3.addEventListener('click',
+(event) =>
+{
+	checkedStars = 3;
+});
+
+btnStar4.addEventListener('click',
+(event) =>
+{
+	checkedStars = 4;
+});
+
+btnStar5.addEventListener('click',
+(event) =>
+{
+	checkedStars = 5;
+});
+
+var btnSubmit = document.getElementById('btnSubmit') as HTMLInputElement;
 
 var responsePoint = document.getElementById('responsePoint');
 
 btnSubmit.addEventListener('click', 
 (event) => 
 {
-	var enteredDate = new Date(dateDOB.value);
-	var currentDate: Date = new Date();
+	var template = getTemplateClone('responseTemplate');
+	var name = txtUserName.value;
+	var comment = txtComments.value;
 
-	let age = currentDate.getFullYear() - enteredDate.getFullYear();
+	var currentDate = new Date();
+	let  dateD = currentDate.getDay().toString();
+	let dateM = currentDate.getMonth().toString();
+	let dateY = currentDate.getFullYear().toString();
+	var dateString = dateD + '/' + dateM + '/' + dateY;
 
-	template.content.getElementById('ageOfCurrentUser')!.innerText = age.toString();
+	template.content.getElementById('responseName')!.innerText = name;
+	template.content.getElementById('responseComment')!.innerText = comment;
+	template.content.getElementById('responseDate')!.innerText = dateString;
+
+	switch(checkedStars)
+	{
+		case(1):
+			template.content.getElementById('responseStar1')!.style.backgroundColor = 'rgb(255,200,0)';
+			break;
+		case(2):
+			template.content.getElementById('responseStar1')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar2')!.style.backgroundColor = 'rgb(255,200,0)';
+			break;
+		case(3):
+			template.content.getElementById('responseStar1')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar2')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar3')!.style.backgroundColor = 'rgb(255,200,0)';
+			break;
+		case(4):
+			template.content.getElementById('responseStar1')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar2')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar3')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar4')!.style.backgroundColor = 'rgb(255,200,0)';
+			break;
+		case(5):
+			template.content.getElementById('responseStar1')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar2')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar3')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar4')!.style.backgroundColor = 'rgb(255,200,0)';
+			template.content.getElementById('responseStar5')!.style.backgroundColor = 'rgb(255,200,0)';
+			break;
+		default:
+			break;
+	}
 
 	responsePoint?.appendChild(template.content);
 })
+
+
 
 function getTemplateClone(templateID: string): HTMLTemplateElement
 {
